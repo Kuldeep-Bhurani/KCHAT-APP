e4e2f5e5129c80319262cfff4c5aef8d0002d08d
Kuldeep-Bhurani <bhuranikuldeep@gmail.com>
2021-05-28 13:09:57 +0530

    Initial Commit

---
+++ js/client.js
@@ -0,0 +1,40 @@
+const socket = io('http://localhost:8000');
+
+const form = document.getElementById('sendContainer');
+const messageInp = document.getElementById('messageInp');
+const messageContainer = document.querySelector('.container');
+var audio = new Audio('ting.mp3');
+
+const append = (message, position) => {
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+    if (position == 'left') {
+        audio.play();
+    }
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const message = messageInp.value;
+    append(`You: ${message}`, 'right')
+    socket.emit('send', message)
+    messageInp.value = ''
+})
+
+const name = prompt('Enter your name to join');
+socket.emit('new-user-joined', name)
+
+socket.on('user-joined', name => {
+    append(`${name} joined the chat`, 'right')
+})
+
+socket.on('recieve', data => {
+    append(`${data.name}: ${data.message}`, 'left')
+})
+
+socket.on('leave', name => {
+    append(`${name} left the chat`, 'left')
+})
